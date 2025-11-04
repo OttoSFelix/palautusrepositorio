3d76ec4865597ede3d55ca70cad096dfa5196a36
OttoSFelix <otto.suokas@helsinki.fi>
2025-11-05 01:00:57 +0200

    muutos mainissa

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,7 +8,7 @@ logger("aloitetaan") # muutos mainissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{summa(x, y)}") # muutos mainissa
+print(f"{erotus(x, y)}") # muutos mainissa
 
 logger("lopetetaan") # lisäys bugikorjaus-branchissa
