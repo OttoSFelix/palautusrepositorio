389ca93b6249b0d15b30654f265cfd0a312d9cbf
OttoSFelix <otto.suokas@helsinki.fi>
2025-11-05 16:44:47 +0200

    osamaara

---
+++ viikko2/git-branch-harjoitus/osamaara.py
@@ -0,0 +1,16 @@
+* remote origin
+  Fetch URL: git@github.com:mluukkai/ohtu-palautukset.git
+  Push  URL: git@github.com:mluukkai/ohtu-palautukset.git
+  HEAD branch: main
+  Remote branches:
+    bugikorjaus              tracked
+    main                     tracked
+    osamaara                 new (next fetch will store in remotes/origin)
+    refs/remotes/origin/tulo stale (use 'git remote prune' to remove)
+  Local branches configured for 'git pull':
+    bugikorjaus merges with remote bugikorjaus
+    main        merges with remote main
+    tulo        merges with remote tulo
+  Local refs configured for 'git push':
+    bugikorjaus pushes to bugikorjaus (up to date)
+    main        pushes to main        (up to date)
