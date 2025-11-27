a9c4466b342c3c8aecddebb3a9288b163eafe131
OttoSFelix <otto.suokas@helsinki.fi>
2025-11-27 18:02:04 +0200

    Revert "tennis tehtävä"

--- viikko5/tennis/src/index.py
+++ viikko5/tennis/src/index.py
@@ -2,12 +2,24 @@ from tennis_game import TennisGame
 
 
 def main():
-    game = TennisGame("p1", "p2")
+    game = TennisGame("player1", "player2")
 
-    while True:
-        point = input('Point won by:')
-        game.won_point(point)
-        print(game.get_score())
+    print(game.get_score())
+
+    game.won_point("player1")
+    print(game.get_score())
+
+    game.won_point("player1")
+    print(game.get_score())
+
+    game.won_point("player2")
+    print(game.get_score())
+
+    game.won_point("player1")
+    print(game.get_score())
+
+    game.won_point("player1")
+    print(game.get_score())
 
 
 if __name__ == "__main__":
